f57c3cdabacecf59728cf21377aa4780842162cd
rickytan <ricky.tan.xin@gmail.com>
2014-03-13 22:54:08 +0800

    Initial Commit

---
+++ RTPlaceholderTextView/RTViewController.h
@@ -0,0 +1,13 @@
+//
+//  RTViewController.h
+//  RTPlaceholderTextView
+//
+//  Created by ricky on 14-3-13.
+//  Copyright (c) 2014年 ricky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RTViewController : UIViewController
+
+@end
---
+++ RTPlaceholderTextView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RTPlaceholderTextView
+//
+//  Created by ricky on 14-3-13.
+//  Copyright (c) 2014年 ricky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RTAppDelegate class]));
+    }
+}
